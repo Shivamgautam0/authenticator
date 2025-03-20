8345a9a7cb2b5df042862690e2234a42d8651fb2
Shivamgautam0 <sggautam15970@gmail.com>
2025-03-20 11:56:37 +0530

    final

--- validate/models.py
+++ validate/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 
 class users(models.Model):
-    useremail = models.EmailField(unique= True, max_length=200)
-    password = models.CharField(max_length=100)
+    username = models.CharField(max_length=50)
+    useremail = models.EmailField(unique= True, max_length=50)
+    password = models.CharField(max_length=16)
     
     def __str__(self):
         return self.useremail
